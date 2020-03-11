ebe585a149cc81fb513788aeaf26268d28726707
Leeseul Kim <dewkim7@gmail.com>
2020-03-10 17:48:05 -0700

    writing a promise

--- news-bot/handler.js
+++ news-bot/handler.js
@@ -1,27 +1,45 @@
-const https = require('https');
 const qs = require('querystring');
 const fetch = require('node-fetch');
 const BOT_TOKEN = process.env.BOT_TOKEN;
+const API_KEY = process.env.API_KEY;
 
 // Verify Url - https://api.slack.com/events/url_verification
-function verify(data, callback) {
+const verify = (data, callback) => {
   if (data.token === BOT_TOKEN) callback(null, data.challenge);
   else callback("verification failed");   
 }
 
+// Fetch News data
+const getNewsData = (event) => {
+  const results = new Promise((resolve, reject) => {
+    const keyword = event.text.trim();
+    fetch(`https://content.guardianapis.com/search?q=${keyword}&api-key=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+      const str = JSON.stringify(data.response.results);
+      resolve(str);
+    })
+    .catch(err => reject(err));
+  });
+}
+
+
 // Post message to Slack - https://api.slack.com/methods/chat.postMessage
-function process(event, callback) {
+const post = (event, callback) => {
   // test the message for a match and not a bot
-  if (!event.bot_id && /(aws|lambda)/ig.test(event.text)) {
-      var text = `<@${event.user}> News Bot Test From Leeseul`;
+  if (!event.bot_id) {
+    const keyword = event.text.trim();
+    getNewsData(event)
+    .then(data => {
+      const text = `<@${event.user}> Here is the recent news\n ${data}`;
       var message = { 
           token: BOT_TOKEN,
           channel: event.channel,
           text: text
       };
-
       var query = qs.stringify(message); // prepare the querystring
-      https.get(`https://slack.com/api/chat.postMessage?${query}`);
+      fetch(`https://slack.com/api/chat.postMessage?${query}`);
+    }, (err) => console.log(err));
   }
 
   callback(null);
@@ -31,7 +49,7 @@ function process(event, callback) {
 exports.bot = (data, context, callback) => {
   switch (data.type) {
       case "url_verification": verify(data, callback); break;
-      case "event_callback": process(data.event, callback); break;
+      case "event_callback": post(data.event, callback); break;
       default: callback(null);
   }
 };
